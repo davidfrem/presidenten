36558b8913b7722ce9260129b66a0de292781709
davidfrem <davidfrem@users.noreply.github.com>
2026-09-13 21:25:45 +0200

    Prevent double-tap zoom during play

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "presidenten-2.3.0-beta.3";
+const CACHE_NAME = "presidenten-2.3.0-beta.4";
 const APP_SHELL = [
   "./",
   "./index.html",
--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-export const APP_VERSION = "2.3.0-beta.3";
+export const APP_VERSION = "2.3.0-beta.4";
